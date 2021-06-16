bceb8d7b58ed80f4083c9bd4c3e9536d11f26cc4
赵越 <zhaoyue11@jd.com>
2021-06-16 13:55:07 +0800

    🐛 模板拉取地址和默认模板名称

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const download = require('download-git-repo')
 
 // 根据用户输入的参数，获取对应的项目模板
 
-const address = 'hailinmo/react-webpack'
+const address = 'hailinmo/react-webpack#main'
 
 cli().then((args) => {
   console.log(args)
-  download(address, `./${args.name}`, function (err) {
+  download(address, `./${args.name || 'ak-template'}`, function (err) {
     console.log(err ? err : 'success')
   })
 })
